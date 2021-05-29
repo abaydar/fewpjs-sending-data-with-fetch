f1f39bc2a869f8342e289c19640b33b41fe64c8d
+ <+>
2021-05-29 17:11:00 -0400

    commit tree 81723aef36fa2e355cbc121a3296718849084c74

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-// Add your code here
+// let formData = {
+//     dogName: "Charlie",
+//     dogBreed: "Bulldog"
+// }
+
+// let configObject = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json"
+//     },
+//     body: JSON.stringify(formData)
+// }
+
+// fetch('http://localhost:3000/dogs', configObject)
+//     .then(function(response){
+//         return response.json()
+//     })
+//     .then(function(object) {
+//         console.log(object)
+//     })
+//     .catch(function(error){
+//         alert("Bad Things!");
+//         console.log(error.message)
+//     })
+
+function submitData(userName, userEmail){
+    let userData = {
+        name: userName,
+        email: userEmail
+    }
+    
+    let userObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"          
+        },
+        body: JSON.stringify(userData)
+    }
+    return fetch('http://localhost:3000/users', userObject)
+        .then(function(response){
+            return response.json()
+        })
+        .then(function(object){
+            document.body.innerHTML += object["id"]
+        } )
+        .catch(function(error){
+            document.body.innerHTML += error.message
+        })
+}
